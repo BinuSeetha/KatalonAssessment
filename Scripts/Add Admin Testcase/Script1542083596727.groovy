05282a3b0b259d2465ce3c10eb57bb8710d8c376
Binu <Binu@BinuK-PC>
2018-11-14 15:27:25 +0530

    second commit

--- Scripts/Add Admin Testcase/Script1542083596727.groovy
+++ Scripts/Add Admin Testcase/Script1542083596727.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-
+//Assignment
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.phptravels.net/admin')
